bc32d847bdb11f6d9fc52b1080afebf637ab153e
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-02-04 20:01:18 +0100

    Wraped the App in ChakraProvider tag & Set the theme and background color of the App & Imported Button, Flex, Input and Text to the App from Chakra Ui & Added inline styles to the imported components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div>
+      <Flex w="100%" h="100vh">
+        <Flex w="100%" flexDir="column" ml="20%" mt="5%" mr="20%" color="white">
+          <Text fontWeight="700" fontSize={30}>
+            Tasks
+          </Text>
+          <Flex mt="2%">
+            <Input w="30%" />
+            <Button ml={5} bg='blue.400'> Add Task</Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </div>
+  );
+};
 
 export default App;
